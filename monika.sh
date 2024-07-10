158caf7642ac3234d1b9401641a0b04eb4f348c8
BrunoGatti <gatti.br15@gmail.com>
2024-07-10 13:05:17 +0200

    commit 3

--- monika.sh
+++ monika.sh
@@ -1,4 +1,13 @@
-python interfaccia_per_comparare.py \
-tutta_la_storia_eval_con_conversazione.xlsx \
-cambia_questa_riga_con_la_cartella_che_contiene_le_immagini \
-annotazioni_monika.xlsx 
+#!/bin/bash
+
+# Arguments
+input_xlsx_path="tutta_la_storia_eval_con_conversazione.xlsx"
+local_image_dir="cambia_questa_riga_con_la_cartella_che_contiene_le_immagini"
+output_xlsx_path="annotazioni_monika.xlsx"
+
+# Install required Python packages
+pip install pandas pillow openpyxl tk
+
+# Run the Python script
+python interfaccia_per_comparare.py "$input_xlsx_path" "$local_image_dir" "$output_xlsx_path"
+
